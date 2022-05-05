52bb4e9f5f69c560d45f684ab0a31c6ae9ecb9c0
Luis Mayta <luis.mayta@gmail.com>
2022-05-05 17:14:15 -0500

    Actualizado a 18

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import PropTypes from 'prop-types'
 
 const CounterApp = ({numero})=>{
+    const [ counter,setCounter ] = useState(numero);
+
+
     const handleAdd = (e)=>{
-        console.log(e)
+        // setCounter(counter+1);
+        setCounter( (c)=>c+1);
     }
+    
+    const handleRes = (e)=>{
+        setCounter(counter-1);
+    }
+    
+    const resetear = (e)=>{
+        setCounter( (c)=>c=numero);
+    }
+
+
+
     return (
         <>
         <h1>Counter App</h1>
-        <p>{ numero }</p>
+        <p>{ counter }</p>
         <button onClick={ handleAdd }>+1</button>
+        <button onClick={ resetear }>reset</button>
+        <button onClick={ handleRes }>-1</button>
         </>
     );
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PrimeraApp from './PrimeraApp';
+import React from 'react'
+import { createRoot } from 'react-dom/client';
 import CounterApp from './CounterApp';
 import './index.css';
 
-// const saludo = <h1>hola mundo</h1>;
 const divRoot = document.querySelector('#root');
+const root = createRoot(divRoot);
 
-ReactDOM.render(<CounterApp numero={123} />,divRoot);
+root.render(<CounterApp numero={123} />);
+//antes 
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// // import PrimeraApp from './PrimeraApp';
+// import CounterApp from './CounterApp';
+// import './index.css';
+
+// // const saludo = <h1>hola mundo</h1>;
+// const divRoot = document.querySelector('#root');
+
+// ReactDOM.render(<CounterApp numero={123} />,divRoot);
