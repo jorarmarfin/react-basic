e8d30ef3aaa0fcfdc9d36d6af9b15f7468634263
Luis Mayta <luis.mayta@gmail.com>
2022-05-05 16:47:28 -0500

    Eventos

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,14 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types'
 
 const CounterApp = ({numero})=>{
+    const handleAdd = (e)=>{
+        console.log(e)
+    }
     return (
         <>
         <h1>Counter App</h1>
         <p>{ numero }</p>
+        <button onClick={ handleAdd }>+1</button>
         </>
     );
 
 }
+
 CounterApp.Prototypes = {
     numero: PropTypes.number
 }
